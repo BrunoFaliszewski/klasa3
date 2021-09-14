c2a9d51db0fa0b98aa7a4feaa445b33ffcc2e7a2
Your Name <you@example.com>
2021-09-14 09:53:48 +0200

    lekcja1

--- lekcja1/zad1.py
+++ lekcja1/zad1.py
@@ -2,19 +2,25 @@ import random
 
 class Guess:
     def __init__(self):
-        self.number = number
-        self.guess = guess
+        self.number = random.randint(1, 100)
+        self.guess = 0
+        self.isCorrect = False
 
-    def
+    def getGuess(self):
+        while(self.isCorrect == False):
+            self.guess = int(input("Zgadnij liczbe od 1 do 100: "))
+            self.checkGuess()
 
     def checkGuess(self):
         if (self.number == self.guess):
             print('zgadłeś!')
-            return True
+            self.isCorrect = True
         elif (self.number > self.guess):
             print('Twoja liczba jest za mała!')
-            return False
+            self.isCorrect = False
         elif (self.number < self.guess):
             print('Twoja liczba jest za duża!')
-            return False
+            self.isCorrect = False
 
+guess = Guess()
+guess.getGuess()
