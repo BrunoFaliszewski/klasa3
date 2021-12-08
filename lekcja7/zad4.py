cd6a2a6298256d06e8d27455365623255c54f617
BrunoFaliszewski <bruno0faliszewski@gmail.com>
2021-12-08 07:12:22 +0100

    lekcja7

--- lekcja7/zad4.py
+++ lekcja7/zad4.py
@@ -4,15 +4,25 @@ from PrimeNumber import prime_number
 def generate_list():
     with open('input.txt', mode='r') as file:
         list = file.readlines()
+        listtoremove = []
         for i in range(len(list)):
-            list[i] = int(list[i].replace("\n", ""))
+            list[i] = list[i].replace("\n", "")
+
+            if list[i].isnumeric():
+                list[i] = int(list[i])
+            else:
+                listtoremove.append(list[i])
+
+        for i in range(len(listtoremove)):
+            list.remove(listtoremove[i])
+
         return list
 
 def generate_file(list):
     with open('out.txt', mode='w') as file:
         for i in range(len(list)):
-            isPrimeNumber = prime_number(i)
-            isPerfectNumber = perfect_number(i)
+            isPrimeNumber = prime_number(list[i])
+            isPerfectNumber = perfect_number(list[i])
             if isPerfectNumber and isPrimeNumber:
                 file.write(f"{list[i]}; l. pierwsza; l. doskonała\n")
             elif isPerfectNumber:
